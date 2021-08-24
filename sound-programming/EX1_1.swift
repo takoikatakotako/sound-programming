7c00c93c798a697a70c6ff614a49e872c8692c4b
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2021-08-25 01:42:59 +0900

    DFTのコードを追加

--- sound-programming/EX1_1.swift
+++ sound-programming/EX1_1.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-//
+// ファイルを読み込み出力する
 func ex1_1(inputFilePath: String, outputFilePath: String) {
     let pcm0 = monoWaveRead(filePath: inputFilePath)
     let pcm1 = pcm0
---
+++ sound-programming/Ex2_4.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+// ファイルをDFT
+func ex2_4(filePath: String) {
+    let pcm0 = monoWaveRead(filePath: filePath)
+
+    let N: Int = 64
+    var xReal: [Double] = [Double](repeating: 0, count: N)
+    var xImag: [Double] = [Double](repeating: 0, count: N)
+    var XReal: [Double] = [Double](repeating: 0, count: N)
+    var XImag: [Double] = [Double](repeating: 0, count: N)
+
+    var wReal: Double
+    var wImag: Double
+    
+    for n in 0..<N {
+        xReal[n] = pcm0.s[n]    // x(n)の実数部
+        xImag[n] = 0.0  // x(n)の虚数部
+    }
+    
+    // DFT
+    for k in 0..<N {
+        for n in 0..<N {
+
+            wReal = cos(2.0 * Double.pi * Double(k) * Double(n) / Double(N) )
+            wImag = -sin(2.0 * Double.pi * Double(k) * Double(n) / Double(N) )
+
+            
+            XReal[k] += wReal * XReal[n] - wImag * xImag[n]
+            XImag[k] += wReal * XImag[n] + wImag * xReal[n]
+
+        }
+    }
+    
+    for k in 0..<N {
+        print("\(k) \(XReal[k]) + \(XImag[k])")
+    }
+    
+    
+}
